fbd4b780993a47d19cefe26f3bf49fdca263377f
Benedikt <benediktclaus@msn.com>
2020-04-21 20:15:57 +0200

    Init t-tests

---
+++ data-simulation.R
@@ -0,0 +1,31 @@
+# Pakete ------------------------------------------------------------------
+library(tidyverse)
+library(haven)
+
+
+# t-Tests -----------------------------------------------------------------
+# Einstichproben-t-Test
+set.seed(20200422)
+ravenclaw <- tibble("intelligence" = rnorm(48, 125, 15)) %>% 
+  rownames_to_column(var = "id") %>% 
+  write_sav(path = "data/ravenclav.sav")
+
+# t-Test für unabhängige Stichproben
+set.seed(20200422)
+chivalry <- tibble(
+  house = as_factor(c("gryffindor", "slytherin")),
+  chivalry = list(rnorm(n = 42, mean = 95, sd = 8), rnorm(n = 38, mean = 45, sd = 9))
+) %>% 
+  unnest(chivalry) %>% 
+  rownames_to_column(var = "id") %>% 
+  write_sav(path = "data/chivalry.sav")
+
+# t-Test für verbundene Stichproben
+set.seed(20200422)
+patience <- tibble(
+  measurement = as_factor(c("begin", "end")),
+  patience = list(rnorm(53, 44, 8), rnorm(53, 46, 9))
+) %>% 
+  unnest(patience) %>% 
+  add_column(id = as.character(rep(1:53, 2)), .before = "measurement") %>% 
+  write_sav(path = "data/patience.sav")
