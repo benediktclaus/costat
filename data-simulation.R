fb918c22634bd81b7e77a7e2c4ef0f8d02af7587
Benedikt <benediktclaus@msn.com>
2020-04-22 16:49:57 +0200

    Structure and complete t-tests

--- data-simulation.R
+++ data-simulation.R
@@ -6,15 +6,15 @@ library(haven)
 # t-Tests -----------------------------------------------------------------
 # Einstichproben-t-Test
 set.seed(20200422)
-ravenclaw <- tibble("intelligence" = rnorm(48, 125, 15)) %>% 
+ravenclaw <- tibble("intelligence" = round(rnorm(48, 125, 15))) %>% 
   rownames_to_column(var = "id") %>% 
   write_sav(path = "data/ravenclav.sav")
 
 # t-Test für unabhängige Stichproben
 set.seed(20200422)
 chivalry <- tibble(
-  house = as_factor(c("gryffindor", "slytherin")),
-  chivalry = list(rnorm(n = 42, mean = 95, sd = 8), rnorm(n = 38, mean = 45, sd = 9))
+  house = as_factor(c("Gryffindor", "Slytherin")),
+  chivalry = list(round(rnorm(n = 42, mean = 95, sd = 8)), round(rnorm(n = 38, mean = 45, sd = 9)))
 ) %>% 
   unnest(chivalry) %>% 
   rownames_to_column(var = "id") %>% 
@@ -23,8 +23,8 @@ chivalry <- tibble(
 # t-Test für verbundene Stichproben
 set.seed(20200422)
 patience <- tibble(
-  measurement = as_factor(c("begin", "end")),
-  patience = list(rnorm(53, 44, 8), rnorm(53, 46, 9))
+  measurement = as_factor(c("Begin", "End")),
+  patience = list(round(rnorm(53, 44, 8)), round(rnorm(53, 46, 9)))
 ) %>% 
   unnest(patience) %>% 
   add_column(id = as.character(rep(1:53, 2)), .before = "measurement") %>% 
